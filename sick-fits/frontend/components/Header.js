3f1b0a98c9afeef2a6b526eb3b59d0291aeee3ae
Mario <mariob360@gmail.com>
2018-12-27 16:42:13 -0800

    Add styled components

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,16 +1,64 @@
 import Nav from './Nav';
+// Link to avoid refresh and take advantage of 
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+  font-style: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  /* Navbar */
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  /* Search Bar */
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+  }
+
+`;
 
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
-      <a href="#">Sick Fits</a>
+      <Logo>
+        <Link href="/">
+          <a >Sick Fits</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
