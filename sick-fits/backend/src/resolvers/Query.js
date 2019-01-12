ee8b61b373df556d56c3b08d63cc228e721ea479
Mario <mariob360@gmail.com>
2019-01-12 15:06:16 -0800

    Create "Items" for Prisma and implement query and mutation resolvers for GraphQL Yoga

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,9 +1,11 @@
-const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-  }, 
+const { forwardTo } = require('prisma-binding');
 
+const Query = {
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
 };
 
 module.exports = Query;
