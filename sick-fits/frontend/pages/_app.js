7ae6f63cb371e968e45f5fd813d9d6018a2079c9
Mario <mariob360@gmail.com>
2019-01-14 22:15:02 -0800

    Setup apollo with React

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,21 +1,38 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData'
 
 class MyApp extends App {
+
+  // Runs before first render
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    // Resolves all queries/mutations and returns data through pageProps
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // This exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       // Container from next.js
       <Container>
+        <ApolloProvider client={apollo}>
         {/* Props, Metadata, Header/Nav */}
         <Page>
           {/* Directs to page depending on name of page */}
-          <Component />
+          <Component {...pageProps}/>
         </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
