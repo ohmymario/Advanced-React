e18cc3f2a600a826a2f373e07bf873fb2a8fdb26
Mario <mariob360@gmail.com>
2019-01-27 16:38:58 -0800

    Create deleteItem Mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+// ctx.db ALL METHODS IN PRISMA.GRAPHQL
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
@@ -21,6 +23,18 @@ const Mutations = {
       data: updates,
       where: { id: args.id }
     }, info)
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+
+    // 1. Find the item 
+    const item = await ctx.db.query.item({ where }, `{ id, title }`)
+
+    // 2. Check if they own the item, or have permissions
+    // TODO
+
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info)
   }
 };
 
