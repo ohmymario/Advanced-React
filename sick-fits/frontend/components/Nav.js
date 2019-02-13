f1b52729845a5c0cb1f286518121bc5e2ed3b416
Mario <mariob360@gmail.com>
2019-02-13 15:44:23 -0800

    Show only relevent links to user depending on login status

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,31 +4,39 @@ import User from './User';
 
 // All nav links here
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {/* {({ data, error, loading })} */}
-      {({ data: { me } }) => {
-        console.log(me);
-        if (me) return <p>{me.name}</p>;
-        return null;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {/* {({ data, error, loading })} */}
+    {({ data: { me } }) => (
+      <NavStyles>
+
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+
+        {me && (
+          <>
+          <Link href="/sell">
+            <a>Sell</a>
+          </Link>
+          <Link href="/orders">
+            <a>Orders</a>
+          </Link>
+          <Link href="/me">
+            <a>Account</a>
+          </Link>
+          </>
+        )}
+
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+
+      </NavStyles>
+    )}
+  </User>
+
 );
 
 export default Nav;
