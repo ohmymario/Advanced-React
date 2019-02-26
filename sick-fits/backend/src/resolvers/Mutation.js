ab4c360d2aaadc9ec98aef748157fdd5f7c67848
Mario <mariob360@gmail.com>
2019-02-25 21:11:50 -0800

    Each Item is now tied to a User when created

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -10,9 +10,18 @@ const { transport, makeANiceEmail } = require('../mail');
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in to do that')
+    }
 
     const item = await ctx.db.mutation.createItem({
       data: {
+        // Relationship between Item and User
+        user: {
+          connect: {
+            id: ctx.request.userId,
+          }
+        },
         ...args
       }
     }, info);
