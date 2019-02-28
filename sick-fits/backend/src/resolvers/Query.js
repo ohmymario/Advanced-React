411162a7f1da8823a5439b875a2b286cc7049022
Mario <mariob360@gmail.com>
2019-02-27 19:20:25 -0800

    Add "Users" Query to backend Able to query information on all users

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -13,6 +14,17 @@ const Query = {
     return ctx.db.query.user({
       where: { id: ctx.request.userId }
     }, info);
+  },
+  async users(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in!')
+    }
+    // 2. Check if the user has the permissions to query all the users
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    // 3. if they have all permissions
+    return ctx.db.query.users({}, info);
   }
 };
 
