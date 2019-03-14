1799ec7b1b22d0daffdb84387f3c100a23643189
Mario <mariob360@gmail.com>
2019-03-14 15:45:49 -0700

    Request cart information when querying user

--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -9,20 +9,31 @@ const CURRENT_USER_QUERY = gql`
       email
       name
       permissions
+      cart {
+        id
+        quantity
+        item {
+          id
+          price
+          image
+          title
+          description
+        }
+      }
     }
   }
 `;
 
-// Pass in user information to children components
+// Pass in currently logged-in user's information to children components
 const User = props => (
   <Query {...props} query={CURRENT_USER_QUERY}>
     {payload => props.children(payload)}
   </Query>
-)
+);
 
 User.propTypes = {
   children: PropTypes.func.isRequired,
-}
+};
 
 export default User;
-export { CURRENT_USER_QUERY }
+export { CURRENT_USER_QUERY };
