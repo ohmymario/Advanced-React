c62fb662c9c9494a10f43d2c47e6e0ed6436695c
Mario <mariob360@gmail.com>
2019-04-21 21:21:46 -0700

    Shallow rendering - html shown without db

---
+++ sick-fits/frontend/__tests__/item.test.js
@@ -0,0 +1,44 @@
+import { shallow } from 'enzyme';
+import ItemComponent from '../components/Item';
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A cool item',
+  price: 5000,
+  description: 'This item is really cool',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg',
+};
+
+describe('<Item/>', () => {
+  it('Renders the image properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+
+    const img = wrapper.find('img');
+    expect(img.props().src).toBe(fakeItem.image);
+    // expect(img.props().alt).toBe(fakeItem.title);
+  });
+
+  it('Renders the Price tag and title properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+
+    const PriceTag = wrapper.find('PriceTag');
+    // dive will shallow render one level deeper
+    expect(PriceTag.dive().text()).toBe('$50');
+
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  });
+
+  it('Renders out the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    // console.log(wrapper.debug());
+
+    const buttonList = wrapper.find('.buttonList');
+    console.log(buttonList.debug());
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.find('Link')).toHaveLength(1);
+    expect(buttonList.find('AddToCart').exists()).toBe(true);
+    expect(buttonList.find('DeleteItem').exists()).toBe(true);
+    // console.log(buttonList.debug());
+  });
+});
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -10,41 +10,44 @@ import formatMoney from '../lib/formatMoney';
 import DeleteItem from './DeleteItem';
 import AddToCart from './AddToCart';
 
-
 export default class Item extends Component {
   static propTypes = {
     item: PropTypes.object.isRequired,
-  }
+  };
 
   render() {
     const { item } = this.props;
-    return <ItemStyles>
+    return (
+      <ItemStyles>
+        {item.image && <img src={item.image} alt={item.title} />}
 
-      {item.image && <img src={ item.image } alt="item.title"/>}
+        <Title>
+          <Link
+            href={{
+              pathname: '/item',
+              query: { id: item.id },
+            }}
+          >
+            <a>{item.title}</a>
+          </Link>
+        </Title>
 
-      <Title>
-        <Link href={{
-          pathname: '/item',
-          query: { id: item.id },
-        }}>
-          <a>{item.title}</a>
-        </Link>
-      </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
 
-      <PriceTag>{formatMoney(item.price)}</PriceTag>
-
-      <p>{item.description}</p>
-      <div className="buttonList">
-        <Link href={{
-          pathname: 'update',
-          query: { id: item.id },
-        }}>
-          <a>Edit ✏️</a>
-        </Link>
-        <AddToCart id={item.id}/>
-        <DeleteItem id={item.id} >Delete this Item</DeleteItem>
-      </div>
-
-    </ItemStyles>
+        <p>{item.description}</p>
+        <div className="buttonList">
+          <Link
+            href={{
+              pathname: 'update',
+              query: { id: item.id },
+            }}
+          >
+            <a>Edit ✏️</a>
+          </Link>
+          <AddToCart id={item.id} />
+          <DeleteItem id={item.id}>Delete this Item</DeleteItem>
+        </div>
+      </ItemStyles>
+    );
   }
 }
