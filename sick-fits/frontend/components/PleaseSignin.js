52cbfb486e91bdaefb77f82e3b7e508b244b6e09
Mario <mariob360@gmail.com>
2019-05-30 13:18:19 -0700

    attempt to fix PleaseSignin,js

--- sick-fits/frontend/components/PleaseSignin.js
+++ sick-fits/frontend/components/PleaseSignin.js
@@ -1,23 +1,22 @@
-import { Query } from 'react-apollo' ;
+import { Query } from 'react-apollo';
 import { CURRENT_USER_QUERY } from './User';
 import Signin from './Signin';
 
 const PleaseSignIn = props => (
   <Query query={CURRENT_USER_QUERY}>
-    {({data, loading}) => {
-      if(loading) return <p>Loading</p>
-
-      // Show Sign in Component if no user
-      if(!data.me) {
-        return <div>
-          <p>Please Sign In before Continuing</p>
-          <Signin/>
-        </div>
+    {({ data, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please Sign In before Continuing</p>
+            <Signin />
+          </div>
+        );
       }
-
-      return props.children
+      return props.children;
     }}
   </Query>
-)
+);
 
 export default PleaseSignIn;
